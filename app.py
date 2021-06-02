301971e42afa5ccb9d5a36109e3f6c26020d54aa
bomcabelo <rodrigues.silva.anderson@gmail.com>
2021-06-02 16:36:08 -0300

    Alteção da Visualização do mapa

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ contrato = pd.read_csv("data-1619262085828.csv")
 
 df_sodf = pd.read_csv('http://dados.df.gov.br/pt_BR/dataset/933d7164-8128-4e12-97e6-208bc4935bcb/resource/d4b9d2aa-ed71-4c7e-8deb-e097590d2cba/download/contratosinesp.csv',
                      sep=',',
-                     encoding='UTF-8',
+                     encoding='iso-8859-1',
                      error_bad_lines=False
                     )
 
