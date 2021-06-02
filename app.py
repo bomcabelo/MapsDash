e43c374b9acf87bd04fea37d0a5aaea24896ac0c
bomcabelo <rodrigues.silva.anderson@gmail.com>
2021-06-02 12:00:14 -0300

    Teste exibição do gráfico

--- app.py
+++ app.py
@@ -24,20 +24,3 @@ contratos = df = pd.read_csv("data-1619262085828.csv")
 st.markdown('### __Base de dados:  Contratos em Andamento__ ')
 st.dataframe(contratos)
 st.markdown('---')
-
-# Extraindo as coordenadas de 18000 registros:
-coordenadas=[]
-for lat,lng in zip(df.lat_documento.values[:18000],df.long_documento.values[:18000]):
-  coordenadas.append([lat,lng])
-
-# Renderizando o mapa com algumas personalizações:
-mapa = folium.Map(location=[-15.788497,-47.879873],zoom_start=11)
-
-# Visualização Gráfica
-st.title('Visualização Gráfica')
-
-# Adicionando os registros no mapa de calor:
- fig = mapa.add_child(plugins.HeatMap(coordenadas))        
- st.plotly_chart(fig)
-
-# Fim
