6cef2bc980cdc65106bb52627561f483b709879c
bomcabelo <rodrigues.silva.anderson@gmail.com>
2021-06-02 11:59:01 -0300

    Teste exibição do gráfico

--- app.py
+++ app.py
@@ -36,34 +36,8 @@ mapa = folium.Map(location=[-15.788497,-47.879873],zoom_start=11)
 # Visualização Gráfica
 st.title('Visualização Gráfica')
 
-cat_data = [
-    ['Phrases'],
-    ['cats are better than dogs'],
-    ['cats eat kibble'],
-    ['cats are better than hamsters'],
-    ['cats are awesome'],
-    ['cats are people too'],
-    ['cats eat mice'],
-    ['cats meowing'],
-    ['cats in the cradle'],
-    ['cats eat mice'],
-    ['cats in the cradle lyrics'],
-    ['cats eat kibble'],
-    ['cats for adoption'],
-    ['cats are family'],
-    ['cats eat mice'],
-    ['cats are better than kittens'],
-    ['cats are evil'],
-    ['cats are weird'],
-    ['cats eat mice']
-]
-    
-gchart.gchart(key="cat_chart", data=cat_data, chartType="WordTree", 
-    width=600, height=400, wordtree={"format": "implicit", "word": "cats"}
-
-
 # Adicionando os registros no mapa de calor:
-# fig = mapa.add_child(plugins.HeatMap(coordenadas))        
-# st.plotly_chart(fig)
+ fig = mapa.add_child(plugins.HeatMap(coordenadas))        
+ st.plotly_chart(fig)
 
 # Fim
